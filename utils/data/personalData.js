ac1eaf0835ee9eb1a993028c9ce0efcaa20d3010
RIKI BANIK <76590963+rikibanik@users.noreply.github.com>
2025-02-07 05:32:23 +0530

    updte

--- utils/data/personalData.js
+++ utils/data/personalData.js
@@ -1,7 +1,7 @@
 export const personalData={
     name: "RIKI BANIK",
     expertise: "BACKEND DEVELOPMENT || SOFTWARE DEVELOPER",
-    resume: "https://drive.google.com/file/d/1bMnyibY9dUZlP_o5EPlH_42AlhWNvd39/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/1vccZmhSVfLUAocaDCAW8tVu0uNNlbCDm/view?usp=sharing",
     github: 'https://github.com/rikibanik',
     linkedIn: 'https://www.linkedin.com/in/rikibanik/'
 }
